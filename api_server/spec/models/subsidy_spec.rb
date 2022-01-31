0c92bb3756478cbb316c828838d4614bfb7e2f20
池垣 隼介 <shunsuke603ikuno@gmail.com>
2022-01-31 08:58:56 +0000

    Add validation

--- api_server/spec/models/subsidy_spec.rb
+++ api_server/spec/models/subsidy_spec.rb
@@ -71,6 +71,79 @@ RSpec.describe Subsidy, type: :model do
       end
     end
   end
+  describe 'supplier_type_must_have_association' do
+    subject { subsidy.valid? }
+
+    let(:subsidy) { build(:subsidy) }
+
+    context 'when supplier_type = ministry' do
+      before { subsidy.supplier_type = 'ministry' }
+
+      context 'when subsidy_ministry exists' do
+        let(:ministry) { create(:ministry) }
+
+        before do
+          subsidy.subsidy_ministry = build(:subsidy_ministry, subsidy: subsidy, ministry: ministry)
+        end
+
+        it 'returns true' do
+          expect(subject).to be_truthy # 成功
+        end
+      end
+      context 'when subsidy_ministry is nil' do
+        it 'returns false' do
+          expect(subject).to be_falsy # 失敗
+        end
+      end
+    end
+    context 'when supplier_type = prefecture' do
+      before { subsidy.supplier_type = 'prefecture' }
+
+      context 'when subsidy_prefecture exists' do
+        let(:prefecture) { create(:prefecture) }
+
+        before do
+          subsidy.subsidy_prefecture = build(:subsidy_prefecture, subsidy: subsidy, prefecture: prefecture)
+        end
+
+        it 'returns true' do
+          expect(subject).to be_truthy # 成功
+        end
+      end
+      context 'when subsidy_prefecture is nil' do
+        it 'returns false' do
+          expect(subject).to be_falsy # 失敗
+        end
+      end
+    end
+    context 'when supplier_type = city' do
+      before { subsidy.supplier_type = 'city' }
+
+      context 'when subsidy_city exists' do
+        let(:city) { create(:city) }
+        let(:prefecture) { create(:prefecture) }
+
+        before do
+          subsidy.subsidy_city = build(:subsidy_city, subsidy: subsidy, city: city)
+          subsidy.subsidy_prefecture = build(:subsidy_prefecture, subsidy: subsidy, prefecture: prefecture)
+        end
+
+        it 'returns true' do
+          expect(subject).to be_truthy # 成功
+        end
+      end
+      context 'when subsidy_city is nil' do
+        it 'returns false' do
+          expect(subject).to be_falsy # 失敗
+        end
+      end
+      context 'when subsidy_city exists but subsidy_prefecture is nil' do
+        it 'returns false' do
+          expect(subject).to be_falsy # 失敗
+        end
+      end
+    end
+  end
   describe 'level_validates' do
     context 'when number<1' do
       it 'returns false' do
