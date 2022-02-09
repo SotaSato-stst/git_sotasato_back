a1110533a51498e992dbcd8236553d127c09b9c5
Yoshimi Kondo <yoshimi.keisui@gmail.com>
2022-02-10 01:30:58 +0900

    Update

--- api_server/app/services/new_subsidy_service.rb
+++ api_server/app/services/new_subsidy_service.rb
@@ -20,11 +20,11 @@ class NewSubsidyService
       hash[:city_id] = city_id
       hash[:prefecture_id] = cities[city_id].prefecture.id if city_id.present?
       hash[:supplier_type] = detect_supplier_type(hash)
-      hash[:created_at] = Time.now
+      hash[:created_at] = @scraping_date.to_time
       hash[:updated_at] = Time.now
       hash
     end
-    SubsidyDraft.insert_all instances
+    SubsidyDraft.insert_all instances if instances.present?
   end
 
   def detect_supplier_type(hash)
@@ -39,17 +39,20 @@ class NewSubsidyService
   end
 
   def new_url_hashes
+    existing_urls = SubsidyDraft.all.pluck(:url) + Subsidy.all.pluck(:url)
     files.map do |file|
       str = file.download
       csv_table = CSV.parse(str.read.force_encoding('UTF-8'), headers: true)
       csv_table.map do |table|
+        next if existing_urls.include?(table['url'])
+
         uri = URI.parse(table['url'])
         {
           source_url_domain: "#{uri.scheme}://#{uri.host}",
           title: table['text'],
           url: table['url']
         }
-      end
+      end.compact
     end.flatten
   end
 
