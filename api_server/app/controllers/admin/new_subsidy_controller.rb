adfa8b532cbe663745d0fb4985976d935d0f868b
Yoshimi Kondo <yoshimi.keisui@gmail.com>
2022-02-22 02:27:23 +0900

    Add notification

--- api_server/app/controllers/admin/new_subsidy_controller.rb
+++ api_server/app/controllers/admin/new_subsidy_controller.rb
@@ -2,6 +2,11 @@ module Admin
   class NewSubsidyController < ApplicationController
     def show
       NewSubsidyService.new(params[:date].to_date).execute!
+      slack = SlackService.new
+      SubsidyDraft.where(created_at: 10.minutes.ago..).each do |subsidy_draft|
+        slack.post_new_subsidy_draft(subsidy_draft)
+      end
+      slack.post_new_subsidies_count
       render json: { success: true }, status: 200
     end
 
