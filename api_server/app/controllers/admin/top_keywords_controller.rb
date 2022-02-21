64aaf13a74152d5b4c4dca76bd6720af3033b34b
Yoshimi Kondo <yoshimi.keisui@gmail.com>
2022-02-21 20:42:28 +0900

    Simplify

--- api_server/app/controllers/admin/top_keywords_controller.rb
+++ api_server/app/controllers/admin/top_keywords_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class TopKeywordsController < ApplicationController
     def index
-      top_keywords = SearchedKeyword.group(:content).count.sort_by { |_, v| -v }.first(50).map(&:first)
+      top_keywords = SearchedKeyword.top_keywords(50)
       render json: { keywords: top_keywords }, status: 200
     end
 
