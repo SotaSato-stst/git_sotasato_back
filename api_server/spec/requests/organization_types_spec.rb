18517c4b110f33242c4d8432cb7f511f4107671f
shunsuke <shunsuke603ikuno@gmail.com>
2022-03-11 03:03:28 +0900

    Add organization_types_test

--- api_server/spec/requests/organization_types_spec.rb
+++ api_server/spec/requests/organization_types_spec.rb
@@ -3,14 +3,7 @@ require 'rails_helper'
 RSpec.describe 'OrganizationTypes API', type: :request do
   describe 'GET /organization_types' do
     let(:user) { create(:user) }
-    let(:organization_types) { ['kojin'] }
-    let!(:subsidy) do
-      create(
-        :subsidy,
-        organization_types: organization_types,
-      )
-    end
-    subject { get '/subsidies' }
+    subject { get '/organization_types' }
 
     before do
       sign_in_with(user)
@@ -18,7 +11,7 @@ RSpec.describe 'OrganizationTypes API', type: :request do
 
     it 'returns correct response' do
       subject
-      expect(json['subsidies'][0]['organization_types']).to eq [{ 'key' => 'kojin', 'name' => '個人事業主' }]
+      expect(json['organization_types'][0]['key']).to eq 'kabu'
     end
   end
 end
