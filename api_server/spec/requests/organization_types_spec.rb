38ae73cb4439f4b5a7e28866e36b6fca1202a29a
shunsuke <shunsuke603ikuno@gmail.com>
2022-03-11 02:32:42 +0900

    Fix

--- api_server/spec/requests/organization_types_spec.rb
+++ api_server/spec/requests/organization_types_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 RSpec.describe 'OrganizationTypes API', type: :request do
   describe 'GET /organization_types' do
     let(:user) { create(:user) }
-    subject { get '/organization_types' }
+    let(:organization_types) { ['kojin'] }
+    let!(:subsidy) do
+      create(
+        :subsidy,
+        organization_types: organization_types,
+      )
+    end
+    subject { get '/subsidies' }
 
     before do
       sign_in_with(user)
@@ -11,7 +18,7 @@ RSpec.describe 'OrganizationTypes API', type: :request do
 
     it 'returns correct response' do
       subject
-      assert_response_schema_confirm(200)
+      expect(json['subsidies'][0]['organization_types']).to eq [{ 'key' => 'kojin', 'name' => '個人事業主' }]
     end
   end
 end
