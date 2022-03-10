7ef9a2b6fe459fd01d2fb7c9c9314da0c6ccbdc7
shunsuke <shunsuke603ikuno@gmail.com>
2022-03-10 23:34:55 +0900

    Add

--- api_server/spec/requests/subsidies_spec.rb
+++ api_server/spec/requests/subsidies_spec.rb
@@ -49,7 +49,6 @@ RSpec.describe 'Subsidies API', type: :request do
 
     it 'returns object' do
       subject
-      binding.pry
       expect(json['subsidies'][0]['title']).to eq 'ものづくり補助金'
       expect(json['subsidies'][0]['url']).to eq 'https://portal.monodukuri-hojo.jp/'
       expect(json['subsidies'][0]['ministry']['name']).to eq '経済産業省'
