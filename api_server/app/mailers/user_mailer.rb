a6b2188d9eca39bfa6f87fa963d14a929bb0ceec
Yoshimi Kondo <yoshimi.keisui@gmail.com>
2022-03-14 02:31:25 +0900

    Remove

--- api_server/app/mailers/user_mailer.rb
+++ api_server/app/mailers/user_mailer.rb
@@ -3,16 +3,6 @@ class UserMailer < ApplicationMailer
     @user = user
     @subsidies = subsidies
     @subject = '新着情報のお知らせ | 補助金ドック'
-    # https://developers.google.com/analytics/devguides/collection/protocol/v1/email
-    tracking_params = {
-      tid: Settings.analytics_id,
-      t: 'event',
-      ec: 'email',
-      ea: 'open',
-      dp: '/email/daily_new_subsidies',
-      dt: @subject
-    }.to_param
-    @tracking_code = "https://www.google-analytics.com/collect?v=1&#{tracking_params}"
     mail(to: @user.email, subject: @subject)
   end
 end
